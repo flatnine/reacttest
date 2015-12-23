22aa814ccc5571826609ce45d0a67175573659f6
Jon Burman <jonathanburman@timeout.com>
2015-12-23 17:52:24 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+let channels = [
+		{name: 'Hardware Support'},
+		{name: 'Software Support'}
+];
+
+class Channel extends React.Component {
+
+	onClick() {
+		console.log("Clicked", this.props.name)
+	}
+
+	render() {
+		return (
+			<li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+		)
+	}
+}
+
+
+class ChannelList extends React.Component {
+	render() {
+		return (
+			<ul>
+				{this.props.channels.map( channel => {
+					return (
+						<Channel name={channel.name}/>
+					)
+				})}
+			
+			</ul>
+			)
+
+	}
+}
+
+
+class ChannelForm extends React.Component {
+	render() {
+		return (
+			<form>
+				<input type='text' />
+			</form>
+		)
+	}
+}
+
+class ChannelSection extends React.Component {
+	render() {
+		return (
+			<div>
+				<ChannelList channels={channels}/>
+				<ChannelForm/>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<ChannelSection />,  document.getElementById('app'));
